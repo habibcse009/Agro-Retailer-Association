cafefa8126292eda1083dba43eee04ef596d9718
Md. Ahasan Habib <27882232+habibcse009@users.noreply.github.com>
2021-01-11 00:26:15 +0600

    Initial Commit

---
+++ app/src/main/java/com/project/agro_retailerapp/AgroTeam/AgroMainActivity.java
@@ -0,0 +1,16 @@
+package com.project.agro_retailerapp.AgroTeam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.agro_retailerapp.R;
+
+public class AgroMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agro_main);
+    }
+}
---
+++ app/src/main/java/com/project/agro_retailerapp/Retailers/RetailerMainActivity.java
@@ -0,0 +1,16 @@
+package com.project.agro_retailerapp.Retailers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.agro_retailerapp.R;
+
+public class RetailerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retailer_main);
+    }
+}
