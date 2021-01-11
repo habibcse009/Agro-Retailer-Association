54d7ef7bb4e1b2fd99ebdd8ea662c30932ff93ca
Md. Ahasan Habib <27882232+habibcse009@users.noreply.github.com>
2021-01-11 15:53:51 +0600

    Login signup added

--- app/src/main/java/com/project/agro_retailerapp/SplashActivity.java
+++ app/src/main/java/com/project/agro_retailerapp/SplashActivity.java
@@ -1,14 +1,37 @@
 package com.project.agro_retailerapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SplashActivity extends AppCompatActivity {
+    public static int splashTimeOut = 2200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.spin_kit);
+        Sprite doubleBounce = new DoubleBounce();
+        progressBar.setIndeterminateDrawable(doubleBounce);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splashTimeOut);
     }
 }
